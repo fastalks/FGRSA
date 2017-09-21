81bc36c64c9754c5de44636be02bfa0df91f4bc4
王发果 <wangfaguo@wfgdeMacBook-Pro.local>
2017-09-21 16:21:45 +0800

    init repo

---
+++ Example/Pods/Target Support Files/FGRSA/FGRSA-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FGRSA : NSObject
+@end
+@implementation PodsDummy_FGRSA
+@end
---
+++ FGRSA/Classes/FGRSA.h
@@ -0,0 +1,16 @@
+
+
+#import <Foundation/Foundation.h>
+
+@interface FGRSA : NSObject
+
+// return base64 encoded string
++ (NSString *)encryptString:(NSString *)str publicKey:(NSString *)pubKey;
+// return raw data
++ (NSData *)encryptData:(NSData *)data publicKey:(NSString *)pubKey;
++ (NSString *)decryptString:(NSString *)str publicKey:(NSString *)pubKey;
++ (NSData *)decryptData:(NSData *)data publicKey:(NSString *)pubKey;
++ (NSString *)decryptString:(NSString *)str privateKey:(NSString *)privKey;
++ (NSData *)decryptData:(NSData *)data privateKey:(NSString *)privKey;
+
+@end
